c4b46e691ae61a65b5bb141fb582c9e396ec6945
yamajyn <j.yama.japan@gmail.com>
2018-01-23 18:00:13 +0900

    アイコンの追加

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,5 @@
 $(function(){
   $(window).scroll(function() {
-    $('#line').height($(window).scrollTop()-$(window).height()/2);
+    $('#line').height($(window).scrollTop()-window.innerHeight * ($(document).height() -$(window).scrollTop())/$(document).height() + 100);
   });
 });
