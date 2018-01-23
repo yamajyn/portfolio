b878a605745c0cfc9cdfc718efa443ddcc3386b0
yamajyn <j.yama.japan@gmail.com>
2018-01-24 01:09:40 +0900

    アニメーションの追加

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,50 @@
 $(function(){
   $(window).scroll(function() {
-    $('#line').height($(window).scrollTop()-window.innerHeight * ($(document).height() -$(window).scrollTop())/$(document).height() + 100);
+    var dHeight = $(document).height();
+    var pos = $(window).scrollTop();
+    $('#line').height(pos-window.innerHeight * ( dHeight-pos-200)/dHeight + 200);
+    $(".effect-right").each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).css({"opacity":"1","transform":"translateX(0)"});
+      } else {
+        $(this).css({"opacity":"0","transform":"translateX(50px)"});
+      }
+    });
+    $(".effect-left").each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).css({"opacity":"1","transform":"translateX(0)"});
+      } else {
+        $(this).css({"opacity":"0","transform":"translateX(-50px)"});
+      }
+    });
+    $(".img-effect-right").each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).css({"transform":"translateX(0)"});
+      } else {
+        $(this).css({"transform":"translateX(50px)"});
+      }
+    });
+    $(".img-effect-left").each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).css({"transform":"translateX(0)"});
+      } else {
+        $(this).css({"transform":"translateX(-50px)"});
+      }
+    });
+  });
+  $('#top-scroll').on('click', function(){
+    $('html,body').animate({scrollTop: $('.content-wrap').height()}, 800, 'swing');
   });
 });
