a58958b76f2042f6f4e6e006c72cd9a98402fbca
yamajyn <j.yama.japan@gmail.com>
2018-01-25 22:11:46 +0900

    アニメーション挙動修正

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,46 +1,48 @@
 $(function(){
   $(window).scroll(function() {
-    var dHeight = $(document).height();
-    var pos = $(window).scrollTop();
+    const dHeight = $(document).height();
+    const pos = $(window).scrollTop();
     $('#line').height(pos-window.innerHeight * ( dHeight-pos-200)/dHeight + 200);
+    const scroll = $(window).scrollTop();
+    const windowHeight = $(window).height();
     $(".effect-right").each(function(){
-      var imgPos = $(this).offset().top;    
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
+      var imgPos = $(this).offset().top;
       if (scroll > imgPos - windowHeight + windowHeight/5){
         $(this).css({"opacity":"1","transform":"translateX(0)"});
       } else {
-        $(this).css({"opacity":"0","transform":"translateX(50px)"});
+        $(this).css({"opacity":"0","transform":"translateX(100px)"});
       }
     });
     $(".effect-left").each(function(){
-      var imgPos = $(this).offset().top;    
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
+      var imgPos = $(this).offset().top;
       if (scroll > imgPos - windowHeight + windowHeight/5){
         $(this).css({"opacity":"1","transform":"translateX(0)"});
       } else {
-        $(this).css({"opacity":"0","transform":"translateX(-50px)"});
+        $(this).css({"opacity":"0","transform":"translateX(-100px)"});
+      }
+    });
+    $(".imgs").each(function(){
+      var imgPos = $(this).offset().top;
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).css({"opacity":"1",});
+      } else {
+        $(this).css({"opacity":"0"});
       }
     });
     $(".img-effect-right").each(function(){
-      var imgPos = $(this).offset().top;    
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
+      var imgPos = $(this).offset().top;
       if (scroll > imgPos - windowHeight + windowHeight/5){
         $(this).css({"transform":"translateX(0)"});
       } else {
-        $(this).css({"transform":"translateX(50px)"});
+        $(this).css({"transform":"translateX(100px)"});
       }
     });
     $(".img-effect-left").each(function(){
-      var imgPos = $(this).offset().top;    
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
+      var imgPos = $(this).offset().top;
       if (scroll > imgPos - windowHeight + windowHeight/5){
         $(this).css({"transform":"translateX(0)"});
       } else {
-        $(this).css({"transform":"translateX(-50px)"});
+        $(this).css({"transform":"translateX(-100px)"});
       }
     });
   });
